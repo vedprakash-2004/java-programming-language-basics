e8a6c94de618d7bfc1dd7c641fe9a09b90ed2351
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-21 23:53:29 +0530

    Sum of a & b (input from User )

--- javaBasics.java
+++ javaBasics.java
@@ -4,9 +4,11 @@ public class javaBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the name");
-        String input = sc.nextLine();
-        System.out.println(input);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = a + b;
+        System.out.println(c);
 
     }
 }
