fe7ef8934e376ea32d67a186606c18f7f5bb9b64
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 00:06:43 +0530

    Area of a circle

--- javaBasics.java
+++ javaBasics.java
@@ -2,13 +2,10 @@ import java.util.Scanner;
 
 public class javaBasics {
 
-    public static void main(String[] args) {
+    public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
-
-        int a = sc.nextInt();
-        int b = sc.nextInt();
-        int c = a * b;
-        System.out.println(c);
-
+        float rad = sc.nextFloat();
+        float area = 3.14f * rad * rad;
+        System.out.println(area);
     }
 }
