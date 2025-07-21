7e2dbfce4b5569dd74144778536ed2664d43626f
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-21 17:49:11 +0530

    very basics pattern

--- javaBasics.java
+++ javaBasics.java
@@ -1,6 +1,9 @@
 public class javaBasics {
     public static void main(String args[]) {
-        System.out.print("hello world");
+        System.out.println("****");
+        System.out.println("***");
+        System.out.println("**");
+        System.out.println("*");
 
     }
 }// boilerplate code
