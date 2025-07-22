40f2f0d45ebb01b0b099ff320d0727d288ee6263
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 16:44:26 +0530

    unaary operator

--- javaBasics.java
+++ javaBasics.java
@@ -1,8 +1,16 @@
 public class javaBasics {
 
     public static void main(String args[]) {
+
+        // int a = 10;
+        // int b = ++a;
+        // System.out.println(a);
+        // System.out.println(b);
+
         int a = 10;
-        int b = 5;
-        System.out.println("add = " + (a + b));
+        int b = a++;
+        System.out.println(a);
+        System.out.println(b);
+
     }
 }
