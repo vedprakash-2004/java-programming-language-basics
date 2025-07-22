bd792c49ecd64f6f6694124564095698420cf82c
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 16:46:31 +0530

    unary operator

--- javaBasics.java
+++ javaBasics.java
@@ -3,12 +3,12 @@ public class javaBasics {
     public static void main(String args[]) {
 
         // int a = 10;
-        // int b = ++a;
+        // int b = --a;
         // System.out.println(a);
         // System.out.println(b);
 
         int a = 10;
-        int b = a++;
+        int b = a--;
         System.out.println(a);
         System.out.println(b);
 
