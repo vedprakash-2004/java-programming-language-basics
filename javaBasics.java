80c127d33389bf656a160a1dcb1cd1557110dde5
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 16:24:20 +0530

    Arithmetic Opretor

--- javaBasics.java
+++ javaBasics.java
@@ -1,13 +1,8 @@
-import java.util.*;
-
 public class javaBasics {
 
     public static void main(String args[]) {
         int a = 10;
-        float b = 20.25f;
-        long c = 25;
-        double d = 30;
-        double ans = a + b + c + d;
-        System.out.println(ans);
+        int b = 5;
+        System.out.println("add = " + (a + b));
     }
 }
