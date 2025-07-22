00500485b93b2a94fce029d388748ed6bebccc22
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 17:24:14 +0530

    solve question

--- javaBasics.java
+++ javaBasics.java
@@ -2,15 +2,12 @@ public class javaBasics {
 
     public static void main(String args[]) {
 
-        int x = 200, y = 50, z = 100;
-        if (x > y && y > z) {
-            System.out.println("Hello");
-        }
-        if (z > y && z < x) {
-            System.out.println("Java");
-        }
-        if ((y + 200) < x && (y + 150) < z) {
-            System.out.println("Hello Java");
-        }
+        int x, y, z;
+        x = y = z = 2;
+        x += y;
+        y -= z;
+        z /= (x + y);
+        System.out.println(x + " " + y + " " + z);
+
     }
 }
