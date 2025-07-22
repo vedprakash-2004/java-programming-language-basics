b8b568e0989c81679a3ef56d165c2d270080dc1b
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 17:20:12 +0530

    solve problem

--- javaBasics.java
+++ javaBasics.java
@@ -2,12 +2,15 @@ public class javaBasics {
 
     public static void main(String args[]) {
 
-        int x = 2, y = 5;
-
-        int exp1 = (x * y / x);
-        int exp2 = (x * (y / x));
-
-        System.out.print(exp1 + " , ");
-        System.out.println(exp2);
+        int x = 200, y = 50, z = 100;
+        if (x > y && y > z) {
+            System.out.println("Hello");
+        }
+        if (z > y && z < x) {
+            System.out.println("Java");
+        }
+        if ((y + 200) < x && (y + 150) < z) {
+            System.out.println("Hello Java");
+        }
     }
 }
