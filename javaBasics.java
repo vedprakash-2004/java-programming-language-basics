9856f8efa185cb211cb45af894717846eb9d9788
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 17:27:47 +0530

    solve question

--- javaBasics.java
+++ javaBasics.java
@@ -2,12 +2,11 @@ public class javaBasics {
 
     public static void main(String args[]) {
 
-        int x, y, z;
-        x = y = z = 2;
-        x += y;
-        y -= z;
-        z /= (x + y);
-        System.out.println(x + " " + y + " " + z);
+        int x = 9, y = 12;
+        int a = 2, b = 4, c = 6;
+
+        int exp = 4 / 3 * (x + 34) + 9 * (a + b * c) + (3 + y * (2 + a)) / (a + b * y);
+        System.out.println(exp);
 
     }
 }
