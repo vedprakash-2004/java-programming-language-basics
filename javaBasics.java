322fec7b9d5282fb283f63b49d100075acbea4a3
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-22 11:39:01 +0530

    Type promotion in Expression

--- javaBasics.java
+++ javaBasics.java
@@ -3,11 +3,10 @@ import java.util.*;
 public class javaBasics {
 
     public static void main(String args[]) {
-        char a = 'a';
-        char b = 'b';
-        System.out.println((int) (b));
-        System.out.println((int) (a));
-        System.out.println(a);
-        System.out.println(b - a);
+        short a = 5;
+        byte b = 25;
+        char c = 'c';
+        byte bt = (byte) (a + b + c);
+        System.out.println(bt);
     }
 }
