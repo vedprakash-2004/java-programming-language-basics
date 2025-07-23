3d7d64638927bf51ff9ae8a64563cf47f6f20eb2
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-23 23:38:07 +0530

    learn conditional  statement

--- javaBasics.java
+++ javaBasics.java
@@ -3,12 +3,17 @@ import java.util.*;
 public class javaBasics {
 
     public static void main(String args[]) {
-        int age = 23;
+        int age = 16;
 
         if (age >= 18) {
             System.out.println("adult, drive, vote");
+
+        }
+        if (age > 13 && age < 18) {
+            System.out.println("teenager");
         } else {
             System.out.println("not adult");
         }
+
     }
 }
