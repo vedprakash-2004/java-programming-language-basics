fe0f23e666a196ebb643356ad90e8e83237de734
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 01:43:43 +0530

    print the largest of 3 numbers

--- javaBasics.java
+++ javaBasics.java
@@ -2,19 +2,18 @@ import java.util.*;
 
 public class javaBasics {
 
-    public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        int income = sc.nextInt();
-        int tax;
+    public static void main(String[] args) {
+        int A = 1;
+        int B = 6;
+        int c = 3;
 
-        if (income < 500000) {
-            tax = 0;
-        } else if (income >= 500000 && income < 1000000) {
-            tax = (int) (income * 0.2);
+        if (A >= B) {
+            System.out.println("A is largest");
+        } else if (c > A && c >= B) {
+            System.out.println("C is largest");
         } else {
-            tax = (int) (income * 0.3);
+            System.out.println("B is largest");
         }
-        System.out.println("your tax is :" + tax);
-
     }
+
 }
