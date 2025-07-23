c3faa6dcad2c7f49719cbb0ab8d97dbf3f34e9eb
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 00:10:06 +0530

    print if number is odd or even

--- javaBasics.java
+++ javaBasics.java
@@ -3,14 +3,12 @@ import java.util.*;
 public class javaBasics {
 
     public static void main(String args[]) {
-        int a = 1;
-        int b = 3;
-
-        if (a >= b) {
-            System.out.println("a is largest of 2");
+        int n = 15;
+        if (n % 2 == 0) {
+            System.out.println("this number is even");
 
         } else {
-            System.out.println("b is largest of 2");
+            System.out.println("this number is odd");
         }
 
     }
