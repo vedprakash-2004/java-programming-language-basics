0119d93b3ca27b8fc06d599c0666fff8e19b7a06
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-23 23:32:20 +0530

    conditional statement

--- javaBasics.java
+++ javaBasics.java
@@ -1,14 +1,14 @@
+import java.util.*;
+
 public class javaBasics {
 
     public static void main(String args[]) {
+        int age = 23;
 
-        int x = 10, y = 5;
-
-        int exp1 = (y * (x / y + x / y));
-        int exp2 = (y * x / y + y * x / y);
-
-        System.out.println(exp1);
-        System.out.println(exp2);
-
+        if (age >= 18) {
+            System.out.println("adult, drive, vote");
+        } else {
+            System.out.println("not adult");
+        }
     }
 }
