b42311473ccfa1327512aa876f9a703933980a76
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 17:40:15 +0530

    learn while loop

--- javaBasics.java
+++ javaBasics.java
@@ -1,17 +1,13 @@
 import java.util.*;
 
 public class javaBasics {
-
     public static void main(String args[]) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number:");
-        int year = sc.nextInt();
+        int counter = 1;
+        while (counter < 100) {
+            System.out.println("hello world");
+            counter++;
 
-        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
-            System.out.println("This year is leap year");
-
-        } else {
-            System.out.println("This year is not leap year");
         }
+        System.out.println("printed HW 100 times");
     }
 }
