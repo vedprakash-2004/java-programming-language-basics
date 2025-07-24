6dea4739041a11af5e5ddc34257fb75e33642b3d
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 16:53:49 +0530

    problem solve conditional statement

--- javaBasics.java
+++ javaBasics.java
@@ -4,13 +4,34 @@ public class javaBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the temperature:");
-        double A = sc.nextDouble();
+        System.out.println("Enter the Number(1-7):");
+        int week = sc.nextInt();
 
-        if (A >= 103.5) {
-            System.out.println("You have a fever");
-        } else {
-            System.out.println("You don't have a fever");
+        switch (week) {
+            case 1:
+                System.out.println("Monday");
+                break;
+            case 2:
+                System.out.println("Tuesday");
+                break;
+            case 3:
+                System.out.println("wednesday");
+                break;
+            case 4:
+                System.out.println("Thursday");
+                break;
+            case 5:
+                System.out.println("Friday");
+                break;
+            case 6:
+                System.out.println("Saturday");
+                break;
+            case 7:
+                System.out.println("sunday");
+                break;
+            default:
+                System.out.println("Invalid input! please enter week number between 1 to 7");
+                break;
         }
     }
 
