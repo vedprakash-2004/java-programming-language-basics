a9d4134c9962159821ef0dc0409750ad937a6c1d
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 16:35:02 +0530

    get a number from the user and print the positive or negative number

--- javaBasics.java
+++ javaBasics.java
@@ -4,32 +4,12 @@ public class javaBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter a:");
-        int a = sc.nextInt();
-        System.out.println("enter b:");
-        int b = sc.nextInt();
-        System.out.println("enter operator");
-        char operator = sc.next().charAt(0);
+        int A = sc.nextInt();
 
-        switch (operator) {
-            case '+':
-                System.out.println(a + b);
-                break;
-            case '-':
-                System.out.println(a - b);
-                break;
-            case '*':
-                System.out.println(a * b);
-                break;
-            case '/':
-                System.out.println(a / b);
-                break;
-            case '%':
-                System.out.println(a % b);
-                break;
-            default:
-                System.out.println("wrong operator");
-
+        if (A >= 0) {
+            System.out.println("positive number");
+        } else {
+            System.out.println("negative number");
         }
     }
 
