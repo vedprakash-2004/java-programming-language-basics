c8e0aaaf7f1daf5908edebb94078456d8fe2e0e7
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 16:16:34 +0530

    Ternary Operator

--- javaBasics.java
+++ javaBasics.java
@@ -3,17 +3,11 @@ import java.util.*;
 public class javaBasics {
 
     public static void main(String[] args) {
-        int A = 1;
-        int B = 6;
-        int c = 3;
+        int number = 4;
 
-        if (A >= B) {
-            System.out.println("A is largest");
-        } else if (c > A && c >= B) {
-            System.out.println("C is largest");
-        } else {
-            System.out.println("B is largest");
-        }
+        // ternary operator
+        String type = ((number % 2) == 0) ? "even" : "odd";
+        System.out.println(type);
     }
 
 }
