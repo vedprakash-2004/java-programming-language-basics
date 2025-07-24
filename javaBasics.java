fa29da6d9942348fc4afb16bbfc87c6c02ddf68e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-24 16:41:12 +0530

    conditional statement problem

--- javaBasics.java
+++ javaBasics.java
@@ -4,12 +4,13 @@ public class javaBasics {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int A = sc.nextInt();
+        System.out.println("Enter the temperature:");
+        double A = sc.nextDouble();
 
-        if (A >= 0) {
-            System.out.println("positive number");
+        if (A >= 103.5) {
+            System.out.println("You have a fever");
         } else {
-            System.out.println("negative number");
+            System.out.println("You don't have a fever");
         }
     }
 
