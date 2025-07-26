56946b078455915dcb589bbc69ce564d39cf0fcd
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-26 18:59:35 +0530

    print HALF-PYRAMID pattern

--- javaBasics.java
+++ javaBasics.java
@@ -1,10 +1,13 @@
 public class javaBasics {
     public static void main(String args[]) {
-        for (int i = 1; i <= 20; i++) {
+        int n = '1';
+
+        for (int i = 1; i <= 4; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print("*");
+                System.out.print(j);
 
             }
+
             System.out.println();
 
         }
