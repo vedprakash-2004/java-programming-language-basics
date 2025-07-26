5c2cd7493523185c43e28b56f44828c13b562089
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-26 19:07:32 +0530

    Print CHARACTER pattern

--- javaBasics.java
+++ javaBasics.java
@@ -1,10 +1,12 @@
 public class javaBasics {
     public static void main(String args[]) {
-        int n = '1';
+        int n = 4;
+        char c = 'A';
 
-        for (int i = 1; i <= 4; i++) {
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j);
+                System.out.print(c);
+                c++;
 
             }
 
