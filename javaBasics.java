f4612814bb6d23e8167ea13f41ec96c91784763e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-27 23:44:53 +0530

    syntax with parameters

--- javaBasics.java
+++ javaBasics.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class javaBasics {
     public static void printHelloWorld() {
         System.out.println("Hello World");
@@ -6,7 +8,15 @@ public class javaBasics {
         return;
     }
 
+    public static void calculateSum() {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int sum = a + b;
+        System.out.println("sum is :" + sum);
+    }
+
     public static void main(String args[]) {
-        printHelloWorld();
+        calculateSum();
     }
 }
