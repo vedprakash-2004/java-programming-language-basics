29389358c1a0b7bd3210658990ed76c740a89e96
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-27 23:35:02 +0530

    syntax-function code Demo

--- javaBasics.java
+++ javaBasics.java
@@ -1,17 +1,9 @@
 public class javaBasics {
+    public static void printHelloWorld() {
+        System.out.println("Hello World");
+    }
+
     public static void main(String args[]) {
-        int n = 4;
-        char c = 'A';
-
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print(c);
-                c++;
-
-            }
-
-            System.out.println();
-
-        }
+        printHelloWorld();
     }
 }
