96e4c448844f123503c3d105126ce39a13d417df
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-28 00:01:43 +0530

    find binomial coefficent

--- javaBasics.java
+++ javaBasics.java
@@ -30,8 +30,17 @@ public class javaBasics {
         return f;
     }
 
-    public static void main(String args[]) {
+    public static int binCoeff(int n, int r) {
+        int fact_n = factorial(n);
+        int fact_r = factorial(r);
+        int fact_nmr = factorial(n - r);
 
-        System.out.println(factorial(4));
+        int binCoeff = fact_n / (fact_r * fact_nmr);
+        return binCoeff;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(binCoeff(5, 2));
+
     }
 }
