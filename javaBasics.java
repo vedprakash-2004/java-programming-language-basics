89a2e4731729e65bcabe17fdbef21c0c8af83828
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-27 23:52:15 +0530

    product of a&b

--- javaBasics.java
+++ javaBasics.java
@@ -16,7 +16,15 @@ public class javaBasics {
         System.out.println("sum is :" + sum);
     }
 
+    public static int multiply(int a, int b) {
+        int product = a * b;
+        return product;
+    }
+
     public static void main(String args[]) {
-        calculateSum();
+        int a = 3;
+        int b = 5;
+        int prod = multiply(a, b);
+        System.out.println("a*b = " + prod);
     }
 }
