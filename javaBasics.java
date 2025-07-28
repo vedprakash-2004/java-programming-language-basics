2fb08882bf8b1a407465f415f03e37f51599c5e4
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-28 23:49:17 +0530

    Overloading using parameters

--- javaBasics.java
+++ javaBasics.java
@@ -39,8 +39,19 @@ public class javaBasics {
         return binCoeff;
     }
 
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+
+    // func to calc sum of 3 nums
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
+    }
+
     public static void main(String args[]) {
-        System.out.println(binCoeff(5, 2));
+        System.out.println(sum(3, 5));
+        System.out.println(sum(5, 2, 1));
 
     }
 }
