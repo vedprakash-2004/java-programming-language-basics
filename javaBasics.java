ab8d143ad61f6c668569ce98a4a4d2f8868ada41
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-30 23:35:22 +0530

    code decimal to binary

--- javaBasics.java
+++ javaBasics.java
@@ -77,30 +77,48 @@ public class javaBasics {
 
     // }
 
-    public static boolean isPrime(int n) {
-        if (n <= 1)
-            return false; // edge case
-        if (n == 2)
-            return true;
+    // public static boolean isPrime(int n) {
+    // if (n <= 1)
+    // return false; // edge case
+    // if (n == 2)
+    // return true;
 
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
+    // for (int i = 2; i <= Math.sqrt(n); i++) {
+    // if (n % i == 0) {
+    // return false;
+    // }
+    // }
+    // return true;
+    // }
 
-    public static void primesInRange(int n) {
-        for (int i = 2; i <= n; i++) {
-            if (isPrime(i)) {
-                System.out.print(i + " ");
-            }
+    // public static void primesInRange(int n) {
+    // for (int i = 2; i <= n; i++) {
+    // if (isPrime(i)) {
+    // System.out.print(i + " ");
+    // }
+    // }
+    // System.out.println(); // for new line after all primes
+    // }
+
+    // public static void main(String[] args) {
+    // primesInRange(20);
+    // }
+
+    public static void decToBin(int n) {
+        int pow = 0;
+        int binNum = 0;
+
+        while (n > 0) {
+            int rem = n % 2;
+            binNum = binNum + (rem * (int) Math.pow(10, pow));
+            pow++;
+            n = n / 2;
         }
-        System.out.println(); // for new line after all primes
+        System.out.println("binary form of " + binNum + "=" + binNum);
+
     }
 
     public static void main(String[] args) {
-        primesInRange(20);
+        decToBin(12);
     }
 }
