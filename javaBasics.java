4188bbb5a9223942c36a8be968f99e82fcf0ea79
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-31 22:28:38 +0530

    inverted & Rotated Half Pyramid

--- javaBasics.java
+++ javaBasics.java
@@ -16,7 +16,23 @@ public class javaBasics {
         }
     }
 
+    public static void inverted_rotated_half_pyramid(int n) {
+        // outer
+        for (int i = 1; i <= n; i++) {
+            // space
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            // stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        hollow_rectangle(4, 5);
+        // hollow_rectangle(4, 5);
+        inverted_rotated_half_pyramid(7);
     }
 }
