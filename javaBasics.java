34e5e859fd43c81e79946c28711f0e6690290b3e
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-31 21:45:25 +0530

    Hollow Rectangle pattern

--- javaBasics.java
+++ javaBasics.java
@@ -1,138 +1,22 @@
-import java.util.*;
-
-import javax.sql.rowset.spi.SyncResolver;
-
 public class javaBasics {
-    // public static void printHelloWorld() {
-    // System.out.println("Hello World");
-    // System.out.println("Hello World");
-    // System.out.println("Hello World");
-    // return;
-    // }
-
-    // public static void calculateSum() {
-    // Scanner sc = new Scanner(System.in);
-    // int a = sc.nextInt();
-    // int b = sc.nextInt();
-    // int sum = a + b;
-    // System.out.println("sum is :" + sum);
-    // }
-
-    // public static int multiply(int a, int b) {
-    // int product = a * b;
-    // return product;
-    // }
-
-    // public static int factorial(int n) {
-    // int f = 1;
-
-    // for (int i = 1; i <= n; i++) {
-    // f = f * i;
-    // }
-    // return f;
-    // }
-
-    // public static int binCoeff(int n, int r) {
-    // int fact_n = factorial(n);
-    // int fact_r = factorial(r);
-    // int fact_nmr = factorial(n - r);
-
-    // int binCoeff = fact_n / (fact_r * fact_nmr);
-    // return binCoeff;
-    // }
-
-    // public static int sum(int a, int b) {
-    // return a + b;
-    // }
-
-    // // func to calc sum of 3 nums
-
-    // public static int sum(int a, int b, int c) {
-    // return a + b + c;
-    // }
-
-    // public static float sum(int a, int b) {
-    // return a + b;
-    // }
-
-    // // func to cal float sum
-
-    // public static float sum(float a, float b) {
-    // return a + b;
-    // }
-
-    // public static boolean isPrime(int n) {
-
-    // for (int i = 2; i <= n - 1; i++) {
-    // if (n % i == 0) {
-    // return false;
-    // }
-
-    // }
-    // return true;
-    // }
-
-    // public static void main(String args[]) {
-    // isPrime(3);
-
-    // }
-
-    // public static boolean isPrime(int n) {
-    // if (n <= 1)
-    // return false; // edge case
-    // if (n == 2)
-    // return true;
-
-    // for (int i = 2; i <= Math.sqrt(n); i++) {
-    // if (n % i == 0) {
-    // return false;
-    // }
-    // }
-    // return true;
-    // }
-
-    // public static void primesInRange(int n) {
-    // for (int i = 2; i <= n; i++) {
-    // if (isPrime(i)) {
-    // System.out.print(i + " ");
-    // }
-    // }
-    // System.out.println(); // for new line after all primes
-    // }
-
-    // public static void main(String[] args) {
-    // primesInRange(20);
-    // }
-
-    // public static void decToBin(int n) {
-    // int pow = 0;
-    // int binNum = 0;
-
-    // while (n > 0) {
-    // int rem = n % 2;
-    // binNum = binNum + (rem * (int) Math.pow(10, pow));
-    // pow++;
-    // n = n / 2;
-    // }
-    // System.out.println("binary form of " + binNum + "=" + binNum);
-
-    // }
-
-    // public static void printS() {
-    // int s = 45;
-    // }
-
-    // public static void main(String[] args) {
-
-    // int a = 45;
-    // System.out.println();
-    // }
+    public static void hollow_rectangle(int totRows, int totCols) {
+        // outer loop
+        for (int i = 1; i <= totRows; i++) {
+            // inner - columns
+            for (int j = 1; j <= totCols; j++) {
+                // cell - (i,j)
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    // boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
 
     public static void main(String[] args) {
-        for (int i = 1, j = 0, k = 4; i <= 5; i++) {
-
-        }
-        int i = 4;
-        System.out.println(i);
+        hollow_rectangle(4, 5);
     }
 }
