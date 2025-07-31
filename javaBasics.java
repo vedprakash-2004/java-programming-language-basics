3d3fa96cb6b8b559f8f79079966b0cb18c15e9d6
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-07-31 22:47:54 +0530

    Floyds Triangle Pattern

--- javaBasics.java
+++ javaBasics.java
@@ -41,9 +41,23 @@ public class javaBasics {
         }
     }
 
+    public static void floyds_triangle(int n) {
+        // outer
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            // inner - how many times will counter be printed
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // hollow_rectangle(4, 5);
         // inverted_rotated_half_pyramid(7);
-        inverted_half_pyramid_withNumbers(7);
+        // inverted_half_pyramid_withNumbers(7);
+        floyds_triangle(4);
     }
 }
