773d0302bc62fb992bbc18b2f3242d8c72ded658
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-08-01 17:34:34 +0530

    Zero_one_triangle

--- javaBasics.java
+++ javaBasics.java
@@ -54,10 +54,25 @@ public class javaBasics {
         }
     }
 
+    public static void zero_one_triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i + j) % 2 == 0) { // even
+                    System.out.print("1");
+                } else {
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         // hollow_rectangle(4, 5);
         // inverted_rotated_half_pyramid(7);
         // inverted_half_pyramid_withNumbers(7);
-        floyds_triangle(4);
+        // floyds_triangle(4);
+        zero_one_triangle(5);
+
     }
 }
