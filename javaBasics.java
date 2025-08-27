cb57dd61674b90ccdc924312e5af4ea550cd4062
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-08-27 15:14:58 +0530

    one question solve

--- javaBasics.java
+++ javaBasics.java
@@ -1,151 +1,26 @@
+import java.util.Arrays;
+
 public class javaBasics {
-    public static void hollow_rectangle(int totRows, int totCols) {
-        // outer loop
-        for (int i = 1; i <= totRows; i++) {
-            // inner - columns
-            for (int j = 1; j <= totCols; j++) {
-                // cell - (i,j)
-                if (i == 1 || i == totRows || j == 1 || j == totCols) {
-                    // boundary cells
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
-        }
-    }
 
-    public static void inverted_rotated_half_pyramid(int n) {
-        // outer
-        for (int i = 1; i <= n; i++) {
-            // space
-            for (int j = 1; j <= n - i; j++) {
-                System.out.print(" ");
-            }
-            // stars
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
+    // Function to reverse array in-place
+    public static void reverse(int[] arr) {
+        int left = 0, right = arr.length - 1;
+        while (left < right) {
+            // swap elements
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
 
-    public static void inverted_half_pyramid_withNumbers(int n) {
-        for (int i = 1; i <= n; i++) {
-            // inner - numbers
-            for (int j = 1; j <= n - i + 1; j++) {
-                System.out.print(j + "  ");
-            }
-            System.out.println();
-        }
-    }
-
-    public static void floyds_triangle(int n) {
-        // outer
-        int counter = 1;
-        for (int i = 1; i <= n; i++) {
-            // inner - how many times will counter be printed
-            for (int j = 1; j <= i; j++) {
-                System.out.print(counter + " ");
-                counter++;
-            }
-            System.out.println();
-        }
-    }
-
-    public static void zero_one_triangle(int n) {
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
-                if ((i + j) % 2 == 0) { // even
-                    System.out.print("1");
-                } else {
-                    System.out.print("0");
-                }
-            }
-            System.out.println();
-        }
-    }
-
-    public static void butterfly(int n) {
-        // 1st half
-        for (int i = 1; i <= n; i++) {
-            // stars -i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            // spaces -x*(n-i)
-            for (int j = 1; j <= 2 * (n - i); j++) {
-                System.out.print(" ");
-            }
-            // stars - i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-        // 2nd half
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            // spaces -x*(n-i)
-            for (int j = 1; j <= 2 * (n - i); j++) {
-                System.out.print(" ");
-            }
-            // stars - i
-            for (int j = 1; j <= i; j++) {
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-
-    }
-
-    public static void solid_rhombus(int n) {
-        for (int i = 1; i <= n; i++) {
-            // space
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.print(" ");
-            }
-            // stars
-            for (int j = 1; j <= n; j++) {
-                System.out.print("*");
-
-            }
-            System.out.println();
-        }
-
-    }
-
-    public static void hollow_rhombus(int n) {
-        for (int i = 1; i <= n; i++) {
-            // spaces
-            for (int j = 1; j <= (n - i); j++) {
-                System.out.println(" ");
-            }
-            // hollow rectangle - stars
-            for (int j = 1; j <= n; j++) {
-                if (i == 1 || i == n || j == 1 || j == n) {
-                    System.out.print("*");
-                } else {
-                    System.out.print(" ");
-                }
-            }
-            System.out.println();
+            left++;
+            right--;
         }
     }
 
     public static void main(String[] args) {
-        // hollow_rectangle(4, 5);
-        // inverted_rotated_half_pyramid(7);
-        // inverted_half_pyramid_withNumbers(7);
-        // floyds_triangle(4);
-        // zero_one_triangle(5);
-        // butterfly(5);
-        // solid_rhombus(10);
-        hollow_rhombus(5);
+        int[] arr = { 1, 2, 3, 4, 5 };
 
+        System.out.println("Original Array: " + Arrays.toString(arr));
+        reverse(arr);
+        System.out.println("Reversed Array: " + Arrays.toString(arr));
     }
 }
