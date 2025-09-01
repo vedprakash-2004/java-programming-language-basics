f72392259bc33d674fdde4208c322de3312c5015
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-09-01 11:15:25 +0530

    find smallest and largest numbers

--- Arrays.java
+++ Arrays.java
@@ -5,17 +5,26 @@ public class Arrays {
 
     public static int getLargest(int numbers[]) {
         int Largest = Integer.MIN_VALUE; // -infinity
+        int smallest = Integer.MAX_VALUE;// infinity
 
         for (int i = 0; i < numbers.length; i++) {
             if (Largest < numbers[i]) {
                 Largest = numbers[i];
             }
+
+            if (smallest > numbers[i]) {
+                smallest = numbers[i];
+
+            }
         }
+        System.out.println("smallest value is :" + smallest);
         return Largest;
+
     }
 
     public static void main(String args[]) {
         int numbers[] = { 1, 2, 6, 3, 5 };
         System.out.println("largest value is :" + getLargest(numbers));
+
     }
 }
