2350a5f35ba8c52af7bf6b4a28f66e2fb7ba5168
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-09-03 06:26:04 +0530

    Pairs in Array

--- Arrays.java
+++ Arrays.java
@@ -1,28 +1,24 @@
+
+import java.util.*;
+
 public class Arrays {
 
-    public static void reverse(int numbers[]) {
-        int first = 0, last = numbers.length - 1;
+    public static void printpairs(int numbers[]) {
+        int tp = 0;
 
-        while (first > last) {
-            // swap
-            int temp = numbers[last];
-            numbers[first] = numbers[last];
-            numbers[last] = numbers[temp];
-
-            first++;
-            last--;
-
+        for (int i = 0; i < numbers.length; i++) {
+            int curr = numbers[i]; // 2, 4, 6, 8, 10
+            for (int j = i + 1; j < numbers.length; j++) {
+                System.out.print("(" + curr + "," + numbers[j] + ")");
+                tp++;
+            }
+            System.out.println();
         }
+        System.out.println("total pairs = " + tp);
     }
 
     public static void main(String args[]) {
         int numbers[] = { 2, 4, 6, 8, 10 };
-        reverse(numbers);
-        // print
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println(numbers[i] + " ");
-        }
-        System.out.println();
-
+        printpairs(numbers);
     }
 }
