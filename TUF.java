829016204716525ecad8cc8069b2b37cd06ecc9b
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-09-09 11:13:57 +0530

    quick revesion largest number

--- TUF.java
+++ TUF.java
@@ -1,18 +1,17 @@
 public class TUF {
 
-    public static void printlinearSearch(int arr[], int key) {
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == key) {
-                System.out.print(i + " ");
+    public static void largestNumber(int nums[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > largest) {
+                largest = nums[i];
             }
-
         }
+        System.out.println(largest);
     }
 
     public static void main(String[] args) {
-        int arr[] = { 2, 4, 6, 8, 10, 12, 14, 16 };
-        int key = 16;
-
-        printlinearSearch(arr, key);
+        int nums[] = { 20, 2, 6, 3, 8 };
+        largestNumber(nums);
     }
 }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,1 @@
-height
+nums
