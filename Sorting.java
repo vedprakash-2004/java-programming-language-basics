f00e2c3cc4d33d3b0bf347e3a3821c7284fe6c9a
vedprakash-2004 <vedprakashjha8473@gmail.com>
2025-09-10 10:48:09 +0530

    learn mergesort

--- Sorting.java
+++ Sorting.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Sorting {
 
     public static void BubbleSorting(int arr[]) {
@@ -48,9 +50,29 @@ public class Sorting {
         }
     }
 
+    public static void countingSortDecreasing(int arr[]) {
+        int largest = Integer.MIN_VALUE;
+        for (int i = 0; i < arr.length; i++) {
+            largest = Math.max(largest, arr[i]);
+        }
+        int count[] = new int[largest + 1];
+        for (int i = 0; i < arr.length; i++) {
+            count[arr[i]]++;
+        }
+        int j = 0;
+        for (int i = count.length - 1; i >= 0; i--) {
+            while (count[i] > 0) {
+                arr[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
     public static void main(String[] args) {
         int arr[] = { 5, 4, 1, 3, 2 };
-        insertionSort(arr);
+        countingSortDecreasing(arr);
+
         printarr(arr);
 
     }
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,4 +1,1 @@
-  // for (int i = 0; i < nums.length; i++) {
-        // System.out.print(nums[i]);
-        // }
-        // System.out.println();
+arr
